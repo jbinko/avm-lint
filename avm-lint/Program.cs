5716265a4bdb4ea86c1a1d518e8a3cc3d90702d4
jbinko <jbinko+github@gmail.com>
2024-11-23 12:52:24 +0100

    Added Dependencies

--- avm-lint/Program.cs
+++ avm-lint/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace avm_lint;
+﻿using System.CommandLine;
+
+namespace avm_lint;
 
 internal sealed class Program
 {
-    static void Main(string[] args)
+    static async Task<int> Main(string[] args)
     {
-        Console.WriteLine("avm-lint");
+        var ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+        var rootCommand = new RootCommand($"Azure Verified Modules Lint [Version {ver}]\nCopyright (c) 2024 Jiri Binko. All rights reserved.");
+        return await rootCommand.InvokeAsync(args);
     }
 }
